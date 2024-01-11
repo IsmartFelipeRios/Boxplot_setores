de6bf5a082a1a9ea5eebc522e1911741d3de970a
IsmartFelipeRios <147547361+IsmartFelipeRios@users.noreply.github.com>
2024-01-11 11:04:24 -0300

    Update Empregabilidade.py

--- Empregabilidade.py
+++ Empregabilidade.py
@@ -72,7 +72,7 @@ if check_password():
     # Título
     st.title('Análise empregabilidade')
 
-    # Título e configurações da barra lateral
+        # Título e configurações da barra lateral
     st.title('Análise de Empregabilidade')
     st.sidebar.title('O que deseja ver?')
     pagina = st.sidebar.radio('Selecione o que deseja ver:', ['Gráfico', 'Dataframe'], index=0)
@@ -82,25 +82,23 @@ if check_password():
     aluno_meta = st.sidebar.radio('Selecione a opção de meta:', ['Estão na meta', 'Não estão na meta', 'Ambas'], index=2)
     ano_termino = st.sidebar.radio('Selecione o ano de término:', ['Alunos que estão no último ano', 'Alunos que estão no penúltimo ano', 'Todos os anos'], index=2)
     oportunidade = st.sidebar.multiselect('Selecione a opção de status da oportunidade:', bd['Status meta'].unique(), default=bd['Status meta'].unique())
-    selected_groups = st.sidebar.multiselect('Selecione o(s) tipo(s) de oportunidade:', bd['Tipo de oportunidade'].unique(), default=bd['Tipo de oportunidade'].unique())
 
-    if aluno_meta != 'Estão na meta':
-        selected_status = st.sidebar.multiselect('Selecione o(s) status:', bd['Status'].unique(), default=bd['Status'].unique())
+    # Filtro 'Selecione o(s) tipo(s) de oportunidade:' com ocultação condicional
+    todas_opcoes = bd['Tipo de oportunidade'].unique()
+    placeholder_selected_groups = st.sidebar.empty()
+
+    if "Oportunidade Não Validada" in oportunidade or "Oportunidade Validada" in oportunidade:
+        selected_groups = placeholder_selected_groups.multiselect(
+            'Selecione o(s) tipo(s) de oportunidade:', 
+            todas_opcoes, 
+            default=todas_opcoes
+        )
     else:
-        status_options = [status for status in bd['Status'].unique() if status not in ["FORMADO", "TRANCAMENTO", "DESLIGADO"]]
-        selected_status = st.sidebar.multiselect('Selecione o(s) status:', status_options, default=status_options)
+        placeholder_selected_groups.empty()
+        selected_groups = todas_opcoes
 
-    # Aplicar filtro com base na opção do aluno_meta
-    if aluno_meta == 'Estão na meta':
-        filtered_data = filtered_data[filtered_data['Classificação_Meta'] == 'Meta']
-    elif aluno_meta == 'Não estão na meta':
-        filtered_data = filtered_data[filtered_data['Classificação_Meta'] != 'Meta']
-
-    # Aplicar filtro com base na opção do ano_termino
-    if ano_termino == 'Alunos que estão no último ano':
-        filtered_data = filtered_data[filtered_data['Ano_termino'] == 2023]
-    elif ano_termino == 'Alunos que estão no pnúltimo ano':
-        filtered_data = filtered_data[filtered_data['Ano_termino'] == 2024]
+    # Filtro 'Selecione o(s) status:' sempre visível
+    selected_status = st.sidebar.multiselect('Selecione o(s) status:', bd['Status'].unique(), default=bd['Status'].unique())
 
     # Aplicar filtro com base nas outras seleções
     filtered_data = filtered_data[
